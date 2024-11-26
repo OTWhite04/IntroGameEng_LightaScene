236b9da6e058ec3d94a5aada93185d79fb38bc57
OTWhite04 <owen.t.white2005@gmail.com>
2024-11-26 12:11:25 -0400

    Adding Toxic Waste Triggers

---
+++ Assets/Scripts/ToxicWasteTrigger.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+
+public class ToxicWasteTrigger : MonoBehaviour
+{
+    public PlayableDirector timeline;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        timeline.Play();
+    }
+}
