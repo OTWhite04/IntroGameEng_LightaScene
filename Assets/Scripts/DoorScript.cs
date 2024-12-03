9aa2d7b03a4c3adf378f4c78eb58b582a566145a
OTWhite04 <owen.t.white2005@gmail.com>
2024-12-03 13:32:13 -0400

    Added Door Timeline

--- Assets/Scripts/DoorScript.cs
+++ Assets/Scripts/DoorScript.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Playables;
 
-public class TempScript : MonoBehaviour
+public class DoorTrigger : MonoBehaviour
 {
     public PlayableDirector timeline;
 
